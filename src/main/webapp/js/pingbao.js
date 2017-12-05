d0960af6f9e9dd7c7ce149e833efc94c55e03279
wangzy <wang200507@126.com>
2017-12-05 11:46:16 +0800

    修改屏保路径

--- src/main/webapp/js/pingbao.js
+++ src/main/webapp/js/pingbao.js
@@ -13,7 +13,7 @@
 		var tempCurrent = new Date().getTime();
 		if(tempCurrent >= (current + timeout)) {
 			var currentURL = window.location.href;
-			window.location.href = "/web/page/pb/index.html?id="+currentURL;
+			window.location.href = "page/pb/index.html?id="+currentURL;
 		}
 	}, 1000)
 })(1000*60*2);
