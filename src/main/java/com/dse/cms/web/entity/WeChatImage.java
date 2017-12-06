53f2e10eeaac01a4928a74656111de370ce8935e
huangpeng <715277829@qq.com>
2017-12-06 15:07:33 +0800

    huangpeng add webchat entity

---
+++ src/main/java/com/dse/cms/web/entity/WeChatImage.java
@@ -0,0 +1,70 @@
+package com.dse.cms.web.entity;
+
+import javax.persistence.*;
+
+@Cacheable
+@Table(name="SSSP_WECHATIMAGE")
+@Entity
+public class WeChatImage {
+
+	private Integer id;
+
+	private WeChat weChat;
+
+	private String url;
+
+	private String imgformat;
+
+	private Byte[] images;
+
+	@GeneratedValue
+	@Id
+	@Column(name = "id", nullable = false, length = 32)
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@JoinColumn(name="WECHAT_ID")
+	@ManyToOne(fetch=FetchType.LAZY)
+	public WeChat getWeChat() {
+		return weChat;
+	}
+
+	public void setWeChat(WeChat weChat) {
+		this.weChat = weChat;
+	}
+
+	@Basic
+	@Column(name = "tb_url", nullable = true, length = 150)
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	@Basic
+	@Column(name = "tb_format", nullable = true,length=10)
+	public String getImgformat() {
+		return imgformat;
+	}
+
+	public void setImgformat(String imgformat) {
+		this.imgformat = imgformat;
+	}
+
+	@Lob
+	@Column(name = "tb_images", nullable = true)
+	public Byte[] getImages() {
+		return images;
+	}
+
+	public void setImages(Byte[] images) {
+		this.images = images;
+	}
+}
