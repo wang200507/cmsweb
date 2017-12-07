aa663b8b7b4d30437fd9d192d7f126405e881c5e
wangzy <wang200507@126.com>
2017-12-07 17:35:22 +0800

    修改js导入

--- src/main/java/com/dse/cms/web/controller/NoticeController.java
+++ src/main/java/com/dse/cms/web/controller/NoticeController.java
@@ -1,12 +1,20 @@
 package com.dse.cms.web.controller;
 
 import com.dse.cms.web.service.NoticeService;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 集团通知controller
